e60c575945457ad23dfb90fc4da4d0a1671edc8d
annukul_sol <annukul.thakran@gobolt.co.in>
2022-10-11 18:12:04 +0530

    Comment removed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./pages/home/Home";
-// import ViewProject from './pages/projects/ViewProject';
 
 const App = () => {
   return (
